b14c2560d508424db251f1f76e9d997c13b1920d
ClassroomAccount <jpepe44@gmail.com>
2020-03-25 12:21:10 -0400

    March 25 madness

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //write nonsense to log
+        Log.e("LOG", "barfoo");
+        Log.e("LOG", "barfoo");
+
     }
 }
